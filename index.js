eb3023999ea2ae03a74bc924ec2fe76c38fda090
Vitor-16 <vitor.slima1616@gmail.com>
2023-03-15 22:33:24 -0300

    desenvolvendo rotas de edição e listagem

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/categoria', (req, res)=>{
 /* FIM DA ROTA DE CADASTRO DE CATEGORIA */
 
 /* INICIO DA ROTA DE LISTAGEM DE CATEGORIA */
-app.get('/listagemCategorias', (req, res)=>{
+app.get('/listagemCategoria', (req, res)=>{
         /* CONFIGURAÇÃO DE REQUISIÇÃO PARA O BACK END */
 
         /* ROTA DO BACK END */
@@ -34,15 +34,46 @@ app.get('/listagemCategorias', (req, res)=>{
     
         /* CHAMAR AXIOS PARA A ROTA DO BACK END */
         axios.get(urlListarCategoria)
-        .then((respnse)=>{
-            console.log(respnse.data);
-            let categoria = respnse.data;
+        .then((response)=>{
+            let categoria = response.data;
+            //console.log(response.data);
             res.render('categoria/listagemCategoria', {categoria});
         })
         .catch((error)=>{
         });
 })
 /* FIM DA ROTA DE LISTAGEM DE CATEGORIA */
+
+/* INICIO DA ROTA QUE LEVA ATÉ  A EDIÇÃO DE CATEGORIA */
+app.get('/editarCategoria/:cod_categoria', (req, res)=>{
+    let {cod_categoria} = req.params;
+
+    /* ROTA DO BACK END */
+    urlListarCategoriaPK = `http://localhost:3000/listarCategoriaPK/${cod_categoria}`;
+
+    /* CHAMAR AXIOS PARA A ROTA DO BACK END */
+    axios.get(urlListarCategoriaPK)
+    .then((response)=>{
+        let categoria = response.data;
+        //console.log(categoria.data);
+        res.render('categoria/editarCategoria.ejs', {categoria});
+    })
+    .catch((error)=>{
+    });
+});
+/* FIM DA ROTA QUE LEVA ATÉ A EDIÇÃO DE CATEGORIA */
+
+/* INICIO DA ROTA DE EDIÇÃO DE CATEGORIA */
+app.post('/editarCategoria', (req, res)=>{
+    //console.log(req.body);
+    let urlEditar = 'http://localhost:3000/alterarCategoria';
+
+    axios.put(urlEditar, req.body)
+    .then((response)=>{
+        res.send('DADO ALTERADO');
+    });
+});
+/* FIM DA ROTA DE EDIÇÃO DE CATEGORIA */
 
 app.listen(3001, ()=>{
     console.log("SERVIDOR FRONTEND RODANDO EM - http://localhost:3001");
